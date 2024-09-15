9e071c473d035719cca2ab342bc4639ec4065bc8
ketan270 <146920220+ketan270@users.noreply.github.com>
2024-09-15 11:51:19 +0530

    Added JS to search for specific news and added fevicon

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener('load', () => fetchNews('India'));
 
+function reload() {
+    window.location.reload();
+}
+
 async function fetchNews(query) {
     const res= await fetch(`${url}${query}&apikey=${API_KEY}`) ;
     const data = await res.json();
@@ -16,8 +20,47 @@ function bindData(articles) {
     articles.forEach(article => {
        if (!article.urlToImage) return;
        const cardClone = newsCardTemplate.content.cloneNode(true);
+       fillDataInCard(cardClone, article);
        cardsContainer.appendChild(cardClone);
-
     });
 
 }
+
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector('#news-img');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newsSource = cardClone.querySelector('#news-source');
+    const newsDesc = cardClone.querySelector('#news-desc');
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+    const date = new Date(article.publishedAt).toLocaleString("en-US",{
+        timeZone: "Asia/Kolkata",
+    });
+
+    newsSource.innerHTML = `${article.source.name} - ${date}`;
+    cardClone.firstElementChild.addEventListener('click', () => {
+        window.open(article.url, '_blank');
+    });
+}
+let curSelectedNav= null;
+
+
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navItems = document.getElementsById(id);
+    curSelectedNav?.classList.remove('active');
+    curSelectedNav = navItem;
+    curSelectedNavItem.classList.add('active');
+}
+
+const searchButton = document.getElementById('search-button');
+const searchText = document.getElementById('search-text');
+searchButton.addEventListener('click', () => {
+    const query = searchText.value;
+    if (!query) return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove('active');
+    curSelectedNav = null;
+});
